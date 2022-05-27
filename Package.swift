791fc32258f1fb387e0f4960dc3662319a789825
Kim Arnett <kim.arnett@deque.com>
2022-05-27 16:44:59 -0400

    add doccarchive, add XCUI 3.0.0

--- Package.swift
+++ Package.swift
@@ -7,23 +7,21 @@ let package = Package(
         .iOS(.v10)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "axeDevToolsUIKit",
-            targets: ["axeDevToolsUIKit"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+            targets: ["axeDevToolsUIKit"]),
+        .library(
+            name: "axeDevToolsXCUI",
+            targets: ["axeDevToolsXCUI"])
     ],
     targets: [
-        /*.binaryTarget(
-            name: "SomeRemoteBinaryPackage",
-            url: "https://url/to/some/remote/xcframework.zip",
-            checksum: "The checksum of the ZIP archive that contains the XCFramework."
-        ),*/
         .binaryTarget(
             name: "axeDevToolsUIKit",
             path: "axeDevToolsUIKit.xcframework"
+        ),
+        .binaryTarget(
+            name: "axeDevToolsXCUI",
+            path: "axeDevToolsXCUI.xcframework"
         )
     ]
 )
